5aeeabcdd6bba775eb4d770568206f3b4ef7f021
EnzoAnandez <enzosarts@gmail.com>
2024-02-17 20:21:33 -0300

    Update

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <time.h>
+
+int main(void)
+{
+/**
+* main - jeje
+* pusheando
+* Return:vuelta
+*/
+int main(void)
+{
+	int n = %d\n
+		if (n > 0)
+			printf("is positive\n");
+	if (n = 0)
+		printf("is zero\n");
+	if (n < 0)
+		printf("is negative\");
+	
+	return (0);
+}
