cbda4b8a342412f8acabda534ece4b28a01a70aa
EnzoAnandez <enzosarts@gmail.com>
2024-02-19 14:22:57 -0300

    Update

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+* main - jeje
+* pusheando
+* Return:vuelta
+*/
+
+int main(void)
+{
+int n;
+
+for (n = 0; n < 9; n++)
+{
+        putchar(n);
+	if (n < 9)
+	{
+	putchar (44);
+	putchar (32);
+	}
+}
+putchar('\n');
+
+return (0);
+
+}
