3868f777545982cca8a7571b8ac29e06b6e1bbd1
EnzoAnandez <enzosarts@gmail.com>
2024-02-19 13:08:22 -0300

    Update

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main - jeje
+* pusheando
+* Return:vuelta
+*/
+
+int main(void)
+{
+
+char n = 'a';
+for (n = 'a' ; n <= 'z'; n++)
+if (n != 'q' && n != 'e')
+putchar(n);
+
+putchar('\n');
+
+return (0);
+}
