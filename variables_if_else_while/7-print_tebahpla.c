5289e942b807eb22f093cd275444dc23397fb3df
EnzoAnandez <enzosarts@gmail.com>
2024-02-19 13:26:24 -0300

    Update

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main - jeje
+* pusheando
+* Return:vuelta
+*/
+
+int main(void)
+{
+
+char n = 'z';
+for (n = 'z' ; n <= 'a'; n--)
+        putchar(n);
+
+putchar('\n');
+
+return (0);
+
+}
