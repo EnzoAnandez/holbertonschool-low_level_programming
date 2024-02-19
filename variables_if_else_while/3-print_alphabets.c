2954634ba441e39616a0f0ba285b654b74a98264
EnzoAnandez <enzosarts@gmail.com>
2024-02-19 11:40:19 -0300

    Update

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main - jeje
+* pusheando
+* Return:vuelta
+*/
+
+int main(void)
+{
+char n = 'a';
+char e = 'A';
+for (n = 'a' ; n <= 'z'; n++)
+for (e = 'A' ; e <= 'Z'; e++)
+putchar(n);
+putchar(e);
+putchar('\n');
+return (0);
+}
