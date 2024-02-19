1380a1832ac2874ec468d0592148e6ec544962ff
EnzoAnandez <enzosarts@gmail.com>
2024-02-19 13:55:28 -0300

    Update

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+* main - jeje
+* pusheando
+* Return:vuelta
+*/
+
+int main(void)
+{
+
+char n = '0';
+char e = 'a';
+for (n = '0' ; n <= '9'; n++)
+        putchar(n);
+
+for (e = 'a' ; e <= 'f'; n++)
+	putchar(e);
+
+putchar('\n');
+
+return (0);
+
+}
