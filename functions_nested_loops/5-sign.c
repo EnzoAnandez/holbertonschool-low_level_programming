558cb6ba2eeee8d65ced0439cb3015a3ea3968a2
EnzoAnandez <enzosarts@gmail.com>
2024-02-20 10:10:06 -0300

    Update

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "main.h"
 #include <ctype.h>
+#include <unistd.h>
 
 /**
  * print_sign - function that prints the sign of a number.
@@ -10,18 +11,27 @@
 
 int print_sign(int n)
 {
+	char e;
+
 	if (n > 0)
 	{
-		putchar('+');
+		e = '+';
+		write(1, &e, 1);
+		write(1, "\n", 1);
 		return (1);
 	}
 	if (n == 0)
 	{
-		putchar('0');
+		e = '0';
+		write(1, &e, 1);
+		write(1, "\n", 1);
 		return (0);
 	}
 	if (n < 0)
 	{
+		e = '-';
+		write(1, &e, 1);
+		write(1, "\n", 1);
 		putchar('-');
 		return (-1);
 	}
