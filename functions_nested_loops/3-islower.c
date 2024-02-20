5135db45e3968b263ea468d8966f5eef741927cf
EnzoAnandez <enzosarts@gmail.com>
2024-02-20 07:51:46 -0300

    Update

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -3,34 +3,28 @@
 
 /**
  * _islower - function that checks for lowercase character.
- * Null
  * Return: zero
  */
 
-int _islower(int c);
-
-int main(void)
-{
-    char test_char = 'n';
-
-    int i = _islower(test_char);
-
-    printf("%d\n", i);
-
-    return (0);
-}
-
 int _islower(int c)
 {
-    if (c >= 'a' && c <= 'z')
-    {
-		putchar('1');
-    }
-    else
-    {
-		putchar('0');
-    }
+	char test_char = 'n';
 
-    putchar('\n');
-    return (c >= 'a' && c <= 'z');
+	int i = _islower(test_char);
+
+	printf("%d\n", i);
+
+	return (0);
+
+	if (c >= 'a' && c <= 'z')
+	{
+		putchar('1');
+	}
+	else
+	{
+		putchar('0');
+	}
+
+	putchar('\n');
+	return (c >= 'a' && c <= 'z');
 }
