da4a3b12f75fdbb41c6f5fed73bbb5c6b7ee9bee
EnzoAnandez <enzosarts@gmail.com>
2024-02-20 20:53:39 -0300

    Update

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - function that adds two integers and returns the result.
+ * @n: first variable
+ * @e: second variable
+ * Return: suma
+ */
+
+int add(int n, int e)
+{
+	return (n + e);
+}
