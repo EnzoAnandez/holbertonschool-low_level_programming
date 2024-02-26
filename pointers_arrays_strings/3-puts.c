cb211a2f31bb9caba6f9b076174804dc2834ac29
EnzoAnandez <enzosarts@gmail.com>
2024-02-25 22:31:10 -0300

    Update

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _puts - function that prints a string, followed by a new line, to stdout.
+ *@str: Variable pointer.
+ */
+
+void _puts(char *str)
+
+{
+	char n[7] = {'s', 't', 'd', 'o', 'u', 't', '\0'};
+}
