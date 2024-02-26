a86e5f2a63aec7b51c7210cff69c4c97ddfdb1a1
EnzoAnandez <enzosarts@gmail.com>
2024-02-26 09:56:04 -0300

    Update

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -7,11 +7,14 @@
 
 void rev_string(char *s)
 {
-	int n;
+	int n, e;
 
-	for (n = 0; s[n] != '\0'; n++)
-	{
-		_putchar (s);
-	}
-	for (n = )
+		for (n = 0; s[n] != '\0'; n++)
+		{}
+
+		for (e = n - 1; e >= 0; e--)
+		{
+			_putchar(s[e]);
+		}
+		_putchar('\n');
 }
