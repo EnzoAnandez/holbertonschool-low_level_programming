321aefd35829dce9a4b72ced7d82bef588d175d9
EnzoAnandez <enzosarts@gmail.com>
2024-02-26 20:42:35 -0300

    Update

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * puts2 - function that prints every other character of a string.
+ * @str: Variable pointer.
+ */
+
+void puts2(char *str)
+{
+	int n;
+
+	for (n = 0; str[n] != '\0'; n = n + 2)
+	{}
+}
