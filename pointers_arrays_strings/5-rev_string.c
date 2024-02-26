4463e957ff52bfe34231242b106fd00d88f08ff3
EnzoAnandez <enzosarts@gmail.com>
2024-02-26 13:35:23 -0300

    Update

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -12,10 +12,10 @@ void rev_string(char *s)
 
 		for (a = 0; s[a] != '\0'; a++)
 		{}
-		for (b = a - 1; b != 0; b--)
+		for (b = a - 1; b > a / 2; b--)
 		{
 				c = s[b];
 				s[b] = s[a - b];
-				s[b] = c;
+				s[a - b] = c;
 		}
 }
