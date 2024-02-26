d6d7691c66c6c2a33eb227ebb3b2e06cf3e0e11a
EnzoAnandez <enzosarts@gmail.com>
2024-02-26 08:17:58 -0300

    Update

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * rev_string - function that reverses a string.
+ * @s: Variable pointer.
+ */
+
+void rev_string(char *s)
+{
+	int n;
+
+	for (n = 0; s[n] != '\0'; n++)
+	{
+		_putchar (s);
+	}
+	for (n = )
+}
