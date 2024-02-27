13c43b19f612eec82886ed7bf30954f492cc7888
EnzoAnandez <enzosarts@gmail.com>
2024-02-27 09:27:19 -0300

    Update

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -8,11 +8,15 @@
 void puts_half(char *str)
 {
 	int n, e;
+		l = *str
+		s = l / 2;
 		for (n = 0; str[n] != '\0'; n++)
 		{}
 	n = n / 2;
 		for (e = n; str[e] != '\0'; e++)
 		{
+			if (e / 2 == 1)
+			{e = e - 1; }
 			_putchar(str[e]);
 		}
 	_putchar('\n');
