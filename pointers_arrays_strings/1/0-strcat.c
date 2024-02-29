9ad35f43a5944d590a488fb14815c118ef501b51
EnzoAnandez <enzosarts@gmail.com>
2024-02-29 08:17:30 -0300

    Update

---
+++ pointers_arrays_strings/1/0-strcat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *_strcat - function that concatenates two strings.
+ * @dest: Variable pointer.
+ * @src: Variable pointer 2.
+ * Return: return the value pointer.
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int n, e;
+
+	for (n = 0; dest[n] != '\0'; e++)
+	{
+		printf("%d", n);
+	}
+	return (dest);
+}
