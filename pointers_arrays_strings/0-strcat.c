64265bf59224a301c60b4ad4006d861d457a6436
EnzoAnandez <enzosarts@gmail.com>
2024-02-29 11:06:39 -0300

    Update

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -22,10 +22,6 @@ char *_strcat(char *dest, char *src)
 	{
 		_putchar(src[a]);
 	}
-	for (e = 0; dest[e] != '\0'; e++)
-	{
-		_putchar(dest[e]);
-	}
 	for (a = 0; src[a] != '\0'; a++)
 	{
 		_putchar(src[a]);
