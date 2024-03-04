b99237a97d4c77326ac6e6f0c86ad8277752bb21
EnzoAnandez <enzosarts@gmail.com>
2024-03-04 19:37:12 -0300

    Update

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *_strncat - function that concatenates two strings.
+ * @dest: Variable pointer 1.
+ * @src: Variable pointer 2.
+ * @n: Variable bytes.
+ * Return: return variable dest.
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	
+}
