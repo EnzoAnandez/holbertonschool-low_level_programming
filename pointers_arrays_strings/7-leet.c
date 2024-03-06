2744924a8104627768ca0ab6a66bd4bb9bf55e72
EnzoAnandez <enzosarts@gmail.com>
2024-03-06 14:32:51 -0300

    Update

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * leet - function that encodes a string into 1337.
+ * @s: Pointer to string s.
+ * Return: Pointer to the string.
+ */
+
+char *leet(char *s)
+{
+	char leet[10] = {'O', 'o', 'L', 'l', 'E', 'e', 'A', 'a', 'T', 't'};
+	int a, e;
+
+	for (a = 0; s[a] != '\0'; a++)
+	{
+		for (e = 0; e < 10; e += 2)
+		{
+			if (s[a] == leet[e] || s[a] == leet[e + 1])
+			{
+				s[a] = '0' + (e / 2) + 1;
+			}
+		}
+	}
+
+	return (s);
+}
